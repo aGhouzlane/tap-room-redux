b5ca4871660839f7309d918c9dab33e805bdf8d7
aGhouzlane <ahmedghouzlane@gmail.com>
2021-07-02 18:47:36 -0400

    Add kegcontrol component

---
+++ src/Components/KegControl.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import KegForm from './KegForm';
+import KegList from './KegList';
+
+class TicketControl extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render(){
+    return (
+      <React.Fragment>
+      </React.Fragment>
+    );
+  }
+
+}
+
+export default TicketControl;
