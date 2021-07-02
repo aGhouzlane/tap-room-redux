e0d901e05d3f07daa2770903207ba65f67475326
aGhouzlane <ahmedghouzlane@gmail.com>
2021-07-02 14:14:34 -0400

    Add home content

---
+++ src/Pages/Home.js
@@ -0,0 +1,13 @@
+import Header from './../Components/Header';
+import Footer from './../Components/Footer';
+
+function Home() {
+  return (
+    <div style={{ minHeight: '100vh' }}>
+      <Header />
+      <Footer />
+    </div>
+  );
+}
+
+export default Home;
