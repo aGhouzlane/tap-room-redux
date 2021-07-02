d1db8cb2400de516f3b7cf87fad62c47715f7f56
aGhouzlane <ahmedghouzlane@gmail.com>
2021-07-02 18:37:05 -0400

    Add reference for the form

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,12 +1,14 @@
 import Header from './../Components/Header';
 import Footer from './../Components/Footer';
 import KegList from '../Components/KegList';
+import KegForm from '../Components/KegForm';
 
 function Home() {
   return (
     <div style={{ minHeight: '100vh' }}>
       <Header />
       <KegList />
+      <KegForm />
       <Footer />
     </div>
   );
