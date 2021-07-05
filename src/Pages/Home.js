8c82500ae8e7b191c97494b324ce30ed707ba04f
aGhouzlane <ahmedghouzlane@gmail.com>
2021-07-05 16:04:53 -0400

    Add conditional redering

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,14 +1,13 @@
 import Header from './../Components/Header';
 import Footer from './../Components/Footer';
-import KegList from '../Components/KegList';
-import KegForm from '../Components/KegForm';
+
+import KegControl from "./../Components/KegControl";
 
 function Home() {
   return (
     <div style={{ minHeight: '100vh' }}>
       <Header />
-      <KegList />
-      <KegForm />
+      <KegControl />
       <Footer />
     </div>
   );
