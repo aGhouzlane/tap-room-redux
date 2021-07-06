ef232772078d1f14bfdeac62ceae6f03178b3d5a
aGhouzlane <ahmedghouzlane@gmail.com>
2021-07-05 21:27:12 -0400

    Add logic to handle selected kegs in kegcontrol

--- src/Components/KegControl.js
+++ src/Components/KegControl.js
@@ -27,16 +27,26 @@ class TicketControl extends React.Component {
     });
   }
 
+  handleChangingSelectedKeg = (id) => {
+    const selectedKeg = this.state.masterKegList.filter(keg => keg.id === id)[0];
+    this.setState({selectedkeg: selectedKeg});
+  }
+
   render() {
     let currentlyVisibleState = null;
     let buttonText = null;
-    if (this.state.formVisibleOnPage) {
+
+    if (this.state.selectedKeg != null) {
+      currentlyVisibleState = <KegDetail keg = {this.state.selectedKeg} />
+      buttonText = "Return to Ticket List";
+    }
+    else if (this.state.formVisibleOnPage) {
       currentlyVisibleState = <KegForm onNewKegCreation={this.handleAddingNewKegToList} />
       buttonText = "Return to Keg List";
     } else {
-      currentlyVisibleState = <KegList kegList={this.state.masterKegList} />
+      currentlyVisibleState = <KegList kegList={this.state.masterKegList} 
+      onKegSelection={this.handleChangingSelectedKeg}/>
       buttonText = "Add keg";
-
     }
     return (
       <React.Fragment>
