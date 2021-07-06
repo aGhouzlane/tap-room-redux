467b8a1f3eb1ce3bdc4fa2d703292c77e4e12396
aGhouzlane <ahmedghouzlane@gmail.com>
2021-07-05 21:38:51 -0400

    Update keg component to handle click details

--- src/Components/Keg.js
+++ src/Components/Keg.js
@@ -1,14 +1,16 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-function Keg(props){
+function Keg(props) {
   return (
     <React.Fragment>
-      <p>{props.name}</p>
-      <p>{props.brand}</p>
-      <p>{props.price}</p>
-      <p>{props.alcoholeContent}</p>
-      <hr/>
+      <div onClick={() => props.whenKegClicked(props.id)}>
+        <p>{props.name}</p>
+        <p>{props.brand}</p>
+        <p>{props.price}</p>
+        <p>{props.alcoholeContent}</p>
+        <hr />
+      </div>
     </React.Fragment>
   );
 }
@@ -17,7 +19,9 @@ Keg.propTypes = {
   name: PropTypes.string.isRequired,
   brand: PropTypes.string.isRequired,
   price: PropTypes.string.isRequired,
-  alcoholeContent: PropTypes.string.isRequired
+  alcoholeContent: PropTypes.string.isRequired,
+  id: PropTypes.string,
+  whenKegClicked: PropTypes.func
 };
 
 export default Keg;
