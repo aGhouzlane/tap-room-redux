ff5c0d6e011e3970032680f95bc318a1e1d5ab17
aGhouzlane <ahmedghouzlane@gmail.com>
2021-07-05 21:43:24 -0400

    Update keg detail

--- src/Components/KegDetail.js
+++ src/Components/KegDetail.js
@@ -1,12 +1,21 @@
 import React from "react";
 
-function KegDetail(props){
+function KegDetail(props) {
+  const { keg } = props;
   return (
     <React.Fragment>
       <h1>Keg Detail</h1>
-      <hr/>
+      <p>{keg.name}</p>
+      <p>{keg.brand}</p>
+      <p>{keg.price}</p>
+      <p>{keg.alcoholeContent}</p>
+      <hr />
     </React.Fragment>
   );
 }
+
+KegDetail.propTypes = {
+  keg: PropTypes.object
+};
 
 export default KegDetail;
