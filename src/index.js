c74fc3847052e7fc1d98aef90691bfdbd2256a82
aGhouzlane <ahmedghouzlane@gmail.com>
2021-07-09 17:07:38 -0400

    Add provider component

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { createStore } from 'redux';
 import reducer from './reducers/keg-list-reducer';
+import { Provider } from 'react-redux';
 
 const store = createStore(reducer);
 
