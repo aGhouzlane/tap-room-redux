a75246af209137d9943dc8e18d0304b51ad8017f
aGhouzlane <ahmedghouzlane@gmail.com>
2021-07-09 17:05:11 -0400

    Add redux imports

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux';
+import reducer from './reducers/keg-list-reducer';
 
 
 ReactDOM.render(
