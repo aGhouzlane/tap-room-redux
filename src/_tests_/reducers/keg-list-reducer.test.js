aaa38230c2232a137247ccc3c83ca4380db5ea5f
aGhouzlane <ahmedghouzlane@gmail.com>
2021-07-09 16:24:07 -0400

    Add a good fail test to add keg to masterkeglist

--- src/_tests_/reducers/keg-list-reducer.test.js
+++ src/_tests_/reducers/keg-list-reducer.test.js
@@ -1,6 +1,37 @@
 import kegListReducer from '../../reducers/keg-list-reducer';
 
 describe('kegListReducer', () => {
+  let action;
+  const kegData = {
+    name: 'Beer',
+    brand: 'Corona',
+    price: '3',
+    alcoholeContent: 'Water',
+    id: 1
+  };
+
+  test('Should successfully add new keg data to masterKegList', () => {
+    const { name, brand, price, alcoholeContent, id } = ticketData;
+    action = {
+      type: 'ADD_KEG',
+      name: name,
+      brand: brand,
+      price: price,
+      alcoholeContent: alcoholeContent,
+      id: id
+    };
+
+    expect(ticketListReducer({}, action)).toEqual({
+      [id]: {
+        names: name,
+        name: name,
+        brand: brand,
+        price: price,
+        alcoholeContent: alcoholeContent,
+        id: id
+      }
+    });
+  });
 
   test('Should return default state if there is no action type passed into the reducer', () => {
     expect(kegListReducer({}, { type: null })).toEqual({});
