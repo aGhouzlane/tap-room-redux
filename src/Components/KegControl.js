59cb264341eed158839178cb3d8e723feffdd25c
aGhouzlane <ahmedghouzlane@gmail.com>
2021-07-09 17:14:53 -0400

    Add import connect to kegcontrol

--- src/Components/KegControl.js
+++ src/Components/KegControl.js
@@ -2,6 +2,7 @@ import React from 'react';
 import NewKegForm from './NewKegForm';
 import KegList from './KegList';
 import KegDetail from './KegDetail';
+import { connect } from 'react-redux';
 
 class KegControl extends React.Component {
 
